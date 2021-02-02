b4552320a15e1ca011ed8e88669774882e3e7876
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2021-02-02 20:32:55 +0700

    Add Layout

---
+++ src/layouts/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+import styled from "styled-components"
+
+const NavbarStyled = styled.div`
+  padding: 1rem;
+  margin: auto 2rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export default () => (
+  <NavbarStyled>
+    <span>Lalal</span>
+  </NavbarStyled>
+)
---
+++ src/layouts/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Navbar from "./Navbar"
+
+export default (props) => (
+  <div className="container">
+    <Navbar />
+    {props.children}
+  </div>
+)
