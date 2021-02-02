dd00d7b9ecfdac90fad7cde4417692c3334c013b
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2021-02-02 21:15:37 +0700

    Add Logo, Background, Style Layouts

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,17 +1,23 @@
 import React from "react"
 import styled from "styled-components"
+import Logo from "../assets/images/logo.png"
 
 const NavbarStyled = styled.div`
   padding: 1rem;
   margin: auto 2rem;
 
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
 `
 
+// const Logo = styled.img`
+//   width: 5rem;
+//   object-fit: cover;
+// `
+
 export default () => (
   <NavbarStyled>
-    <span>Lalal</span>
+    <img src={Logo} width={250} alt="logo" />
   </NavbarStyled>
 )
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,42 @@
 import React from "react"
 import Navbar from "./Navbar"
+import styled from "styled-components"
+import BgBlack from "../assets/images/body_bg.png"
+import BgGrey from "../assets/images/container_bg.png"
+
+const BgStyled = styled.div`
+  background-image: url(${(props) => props.img});
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+`
+
+const ContainerStyled = styled.div`
+  width: 75%;
+  min-height: 100vh;
+  background-color: white;
+`
+
+const ChildrenStyled = styled.div`
+  margin-top: 3rem;
+`
+
+const ContentStyled = styled.div`
+  padding: 1rem;
+  width: 80%;
+  min-height: 100vh;
+  background-color: white;
+`
 
 export default (props) => (
-  <div className="container">
-    <Navbar />
-    {props.children}
-  </div>
+  <BgStyled img={BgBlack}>
+    <ContainerStyled>
+      <BgStyled img={BgGrey}>
+        <ContentStyled>
+          <Navbar />
+          <ChildrenStyled>{props.children}</ChildrenStyled>
+        </ContentStyled>
+      </BgStyled>
+    </ContainerStyled>
+  </BgStyled>
 )
